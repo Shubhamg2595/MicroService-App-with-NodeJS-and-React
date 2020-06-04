880d2a2e2bab4ed407d0029f73b4dd11548df960
Shubham Gupta <shubhamg2595@gmail.com>
2020-06-04 15:54:36 +0530

    event emitted

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
@@ -24,7 +25,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -32,6 +33,14 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios.post("http://localhost:3005/events", {
+    type: "Post Created",
+    date: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
