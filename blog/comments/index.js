31a41d9231c1a6c92c292bc9b5856beae851ca8d
Shubham Gupta <shubhamg2595@gmail.com>
2020-06-04 16:57:59 +0530

    receiving events

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,22 +2,12 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-//   next();
-// });
 
 const commentsByPostId = {};
 
@@ -25,7 +15,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const { id } = req.params;
 
   const commentId = randomBytes(4).toString("hex");
@@ -37,7 +27,22 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[id] = comments;
 
+  await axios.post("http://localhost:3005/events", {
+    type: "Comment added",
+    date: {
+      id: commentId,
+      postId: id,
+      content,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(3001, () => {
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -45,6 +45,15 @@ app.post("/posts", async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+
+
+
 app.listen(3000, () => {
   console.log("Posts service started on Port 3000");
 });
