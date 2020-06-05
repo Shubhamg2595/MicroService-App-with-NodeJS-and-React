072d0c2e8dd13c4e0edf9bcafa8d77f60ded68a4
Shubham Gupta <shubhamg2595@gmail.com>
2020-06-05 17:46:01 +0530

    important notes added

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -35,3 +35,8 @@ app.post("/events", async (req, res) => {
 app.listen(3004, () => {
   console.log("moderation service running on port 3004");
 });
+
+//? why i created moderation service : because in long run there can be 'n; number of cases where we will need to update a specific comment in post like edit comment, flag a comment, upvote and downvote. Now all these features can be handled using moderation service. so even if this service fails, our commenting feature will work perfectly.
+
+
+// ? to check usefulness of moderation service, just close this service and use the app
